280b8e6f9ab2f9ff1530bd5067673c4d92f35537
Yuliya Ausiannikava <yuliyaausiannikava@gmail.com>
2023-07-05 21:05:02 +0300

    Dog new field + constructor

--- src/main/java/model/Dog.java
+++ src/main/java/model/Dog.java
@@ -1,2 +1,25 @@
-package model;public class Dog {
+package model;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+public class Dog {
+    private String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dog dog = (Dog) o;
+        return Objects.equals(name, dog.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+
 }
