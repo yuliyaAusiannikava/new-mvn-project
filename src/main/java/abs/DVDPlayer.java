9bd84a655f4fcf5d945b93b8abdabc084ba2f437
Yuliya Ausiannikava <yuliyaausiannikava@gmail.com>
2023-07-10 19:32:34 +0300

    Test

--- src/main/java/abs/DVDPlayer.java
+++ src/main/java/abs/DVDPlayer.java
@@ -1,15 +1,20 @@
 package abs;
 
 public class DVDPlayer extends Player implements Playable, Test {
+
     @Override
     public void play() {
         System.out.println("Start playing DVD Disk...");
     }
 
     @Override
+    public void play(int numberOfSongs) {
+
+    }
+
+    @Override
     public void stop() {
         System.out.println("Stop playing DVD Disk...");
-
     }
 
     @Override
--- src/main/java/abs/TypePlayer.java
+++ src/main/java/abs/TypePlayer.java
@@ -1,9 +1,13 @@
 package abs;
 
-public class TypePlayer extends Player implements Playable{
+public class TypePlayer extends Player implements Playable {
     @Override
     public void play() {
         System.out.println("Start type record composition");
+    }
+
+    @Override
+    public void play(int numberOfSongs) {
     }
 
     @Override
